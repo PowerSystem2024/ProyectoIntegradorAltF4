25d0f16dbecd5b3f8360a1e4c0816d9172ad40d9
Horacio Quiles <quileshoxar@gmail.com>
2025-11-10 20:19:03 -0300

    config deploy 17

--- backend/db.js
+++ backend/db.js
@@ -3,11 +3,11 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
     
-    host: process.env.MYSQL_HOST,      
-    user: process.env.MYSQL_USER,           
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,       
-    port: process.env.MYSQL_PORT
+    host: process.env.MYSQL_HOST || mysql-hfu5.railway.internal,      
+    user: process.env.MYSQL_USER || root,           
+    password: process.env.MYSQL_PASSWORD || '',
+    database: process.env.MYSQL_DATABASE || railway,       
+    port: process.env.MYSQL_PORT || 3306
     
 });
 
