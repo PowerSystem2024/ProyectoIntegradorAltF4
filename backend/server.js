269517877153f7fb9da93b45990d8606cede1518
Horacio Quiles <quileshoxar@gmail.com>
2025-11-10 14:55:50 -0300

    config deploy

--- backend/server.js
+++ backend/server.js
@@ -196,7 +196,7 @@ app.post("/save_order", async (req, res) => {
 });
 
 // RUTA CATCH-ALL PARA SERVIR EL FRONTEND
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(frontendPath, 'index.html'));
 });
 
